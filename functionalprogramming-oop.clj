19344b3d95b838b51143794cb162b2dd8a8b46b2
Naveen Srinivasan <naveensrinivasan@yahoo.com>
2013-07-03 10:13:37 -0400

    solutions to problems

---
+++ functionalprogramming-oop.clj
@@ -0,0 +1,5 @@
+; Excercise 4
+;Implement add-squares.
+(apply + (map (fn [x] (* x x)) '(1 2 5)))
+
+
--- solutions.clj
+++ solutions.clj
@@ -21,3 +21,27 @@
 ;problem 8
 (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d)))
 (= #{:a :b :c :d} (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+;problem 9
+(= #{1 2 3 4} (conj #{1 4 3} 2))
+
+;problem 10
+(= 20  ((hash-map :a 10, :b 20, :c 30) :b))
+(= 20 (:b {:a 10, :b 20, :c 30}))
+
+; problem 11
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
+
+;problem 12
+(= 3 (first '(3 2 1)))
+(= 3 (second [2 3 4]))
+(= 3 (last (list 1 2 3)))
+
+;probelm 12
+(= 3 (first '(3 2 1)))
+
+;probelm 24
+(= (apply +  [1 2 3]) 6)
+
+;problem 25
+(= (filter #(= (mod %1 2) 1) #{1 2 3 4 5} ) '(1 3 5))
